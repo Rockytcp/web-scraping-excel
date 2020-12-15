5965319a350e077ed8f749eb17fb56468adc74e2
Rockytcp <56940852+Rockytcp@users.noreply.github.com>
2020-12-15 12:59:50 -0300

    3

--- web-scraping.py
+++ web-scraping.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-
-url = "https://www.cip-bancos.org.br/DadosEstrategicos/EvolPagsMesTot.html"
-
-id_tabela = "oReportCell"
-
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-tabela = soup.find(atttrs={'id': id_tabela})
-
-df = pd.read_html(str(tabela))
-
-print(df)
+import csv
 
 
-#df = pd.DataFrame(tabela, columns = ['Mês', 'D.U', 'Quantidade Média Diária', 'Valor em R$ mil Média Diária', 'Quantidade Consolidado', 'Valor em R$ mil Consolidado'])
+def exportar(url):
+    url = url
 
-#df.to_excel (r'C:\Users\gbp\Desktop\Trabalho\Web Scraping\export_dataframe.xlsx', index = False, header=True)
+    id_tabela = "oReportCell"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    tabela = soup.find(attrs={'id': id_tabela})
+
+    df = pd.read_html(str(tabela))[0]
+    #print(df)
+    pd.DataFrame(tabela)
+    df.to_excel ("teste.xlsx")
+
+    
+
+tabela1 = exportar("https://www.cip-bancos.org.br/DadosEstrategicos/EvolPagsMesTot.html")
+#tabela2 = exportar("https://www.cip-bancos.org.br/DadosEstrategicos/EvolPags.html")
