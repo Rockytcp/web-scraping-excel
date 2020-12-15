f5507942a9d5cc8afd0d50944ec4b9b1e0f67eb2
Rockytcp <56940852+Rockytcp@users.noreply.github.com>
2020-12-15 09:02:33 -0300

    3

--- web-scraping.py
+++ web-scraping.py
@@ -9,6 +9,13 @@ id_tabela = "oReportCell"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-tabela = soup.find('table', atttrs={'id': id_tabela})
+tabela = soup.find(atttrs={'id': id_tabela})
 
 df = pd.read_html(str(tabela))
+
+print(df)
+
+
+#df = pd.DataFrame(tabela, columns = ['Mês', 'D.U', 'Quantidade Média Diária', 'Valor em R$ mil Média Diária', 'Quantidade Consolidado', 'Valor em R$ mil Consolidado'])
+
+#df.to_excel (r'C:\Users\gbp\Desktop\Trabalho\Web Scraping\export_dataframe.xlsx', index = False, header=True)
